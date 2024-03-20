c3e12cb7ea0c3f486b7f4b0fb0026e75b84c39e0
Reemasalam <reemasalam6554@gmail.com>
2024-03-20 09:24:56 +0530

    almost done

--- lib/Screens/PickupListScreen/PickupListScreen.dart
+++ lib/Screens/PickupListScreen/PickupListScreen.dart
@@ -25,21 +25,7 @@ class PickupListScreen extends StatelessWidget {
             ],
           ),
         ),
-        CircleAvatar(
-          radius: 6.w,
-          backgroundColor: Color(0xff036163),
-          child: SizedBox(
-            child: InkWell(
-              onTap: () {
-                showDialog(
-                    context: context,
-                    builder: (context) => MrfCard(
-                        //type: type,
-                        ));
-              },
-            ),
-          ),
-        ),
+        
       ],
     );
   }
